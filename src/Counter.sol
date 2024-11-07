23e8cbbb456dabdebc2a99da77a7af8cc794329d
Claudia <claudiabarcelovaldes40@gmail.com>
2024-11-07 12:05:26 +0100

    feat: configure oz upgrades

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Counter is UUPSUpgradeable {
+    address owner;
     uint256 public number;
+
+    function initialize() public initializer {
+        number = 0;
+        owner = msg.sender;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -11,4 +19,10 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override {}
+
+    uint256[49] private __gap;
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
 contract CounterTest is Test {
     Counter public counter;
 
@@ -20,5 +22,12 @@ contract CounterTest is Test {
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function test_upgradeability() public {
+        address proxy = Upgrades.deployUUPSProxy(
+            "Counter.sol",
+            abi.encodeCall(Counter.initialize, ())
+        );
     }
 }
